f36ff77a1b46dda099961679850457c7c9cd1e03
João Victor Alves Rodrigues <joaovictorar1@gmail.com>
2024-08-21 16:46:10 -0300

    python letras

--- cursos python/curso em video/aulas/aula09.py
+++ cursos python/curso em video/aulas/aula09.py
@@ -13,7 +13,7 @@ print(len(frase)) #comprimento da frase
 print(frase.count('a',0, 10)) #contador de letra
 print(frase2.find('deo'))
 print(frase2.find('android'))
-print('curso' in frase2)
+print('curso' in frase2) #posição da palavra
 print(frase2.replace('python', 'android'))
 print(frase.upper())
 print(frase.lower())
@@ -22,3 +22,7 @@ print(frase.title()) #capitalize palavra por palavra
 print(frase2.strip()) #remove espaços inúteis no inicio e fim da frase
 print(frase2.rstrip()) #somente lado direito de right 
 print(frase2.lstrip()) #somente lado esquerdo de left
+print(frase.split()) #vai separar as frases em palavras individuais
+print('-'.join(frase)) #inclui entre os espaços
+print("""Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, aliquid, perferendis recusandae, eveniet quasi non corporis perspiciatis maxime quo ad quisquam saepe possimus ipsum vitae illo quibusdam nam voluptatum voluptas!
+</body>""") #""" escreve o texto como ele é
---
+++ cursos python/curso em video/aulas/aula09pt2.py
@@ -0,0 +1,9 @@
+frase = '  Curso em Video Python'
+print(frase.lower().count('o'))
+print(len(frase.strip()))
+print(frase)
+frase = frase.replace('Python', 'Android')
+print(frase)
+print(frase.find('Video'))
+dividido = frase.split()
+print(dividido[2] [3])
